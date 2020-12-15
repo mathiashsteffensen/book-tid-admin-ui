7a9f8106198707d3fdd60a20e10e03833d3132d9
Mathias <mathias@mathiashsteffensen@protonmail.com>
2020-12-15 16:07:03 +0100

    styles fixed

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,6 @@
 module.exports = {
   plugins: [
     'tailwindcss',
-    'autoprefixer',
-    [
-      '@fullhuman/postcss-purgecss',
-      {
-        content: [
-            './src/pages/**/*.{js,jsx,ts,tsx}',
-            './src/components/**/*.{js,jsx,ts,tsx}',
-            './node_modules/react-bootstrap/**/*.{js,jsx,ts,tsx}'
-        ],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-      }
-    ],
+    'autoprefixer'
   ],
 };
