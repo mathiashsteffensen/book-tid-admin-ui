4969beb9c19216ebd582a9526ab541695cdac103
Mathias <mathias@mathiashsteffensen@protonmail.com>
2020-12-20 22:44:09 +0100

    modified CSS purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: [
     'tailwindcss',
-    'autoprefixer'
+    'autoprefixer',
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+            './src/pages/**/*.{js,jsx,ts,tsx}',
+            './src/components/**/*.{js,jsx,ts,tsx}'
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      }
+    ],
   ],
 };
