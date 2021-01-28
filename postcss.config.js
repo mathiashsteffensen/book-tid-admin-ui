f6143966f56b6d33556bb6bfbcde19857c8de608
Mathias <mathias@mathiashsteffensen@protonmail.com>
2021-01-28 22:25:10 +0100

    updates and optimizations

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,6 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
   plugins: {
-    '@fullhuman/postcss-purgecss': {
-        content: [
-        './src/pages/*.js',
-        './src/pages/**/*.js',
-        './src/components/*.js',
-        './src/components/**/*.js',
-        './node_modules/react-bootstrap/**/*.js',
-      ]
-    },
     'tailwindcss': {},
     'autoprefixer': {},
-
-    
-
   },
 };
