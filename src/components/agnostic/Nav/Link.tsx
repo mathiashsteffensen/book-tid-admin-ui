dcac567280baaebf24efed28b98b0ef537d6dba9
Mathias <mathias@mathiashsteffensen@protonmail.com>
2021-02-18 18:38:22 +0100

    An app store for premium features has been added

--- src/components/agnostic/Nav/Link.tsx
+++ src/components/agnostic/Nav/Link.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-//@ts-nocheck
-export const Link = React.forwardRef(({ className, children, ...otherProps }: React.HTMLAttributes<HTMLAnchorElement>, ref) => {
+
+export const Link = ({ className, children, ...otherProps }: React.LinkHTMLAttributes<any>) => {
     return (
         <a
-            ref={ref}
             className={`nav-link ${className}`}
             {...otherProps}
         >
             {children}
         </a>
     )
-})
+}
